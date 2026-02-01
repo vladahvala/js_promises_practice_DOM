c8d70fa787a077a24942586a1ae1c5adde31c644
vladahvala <80634246+vladahvala@users.noreply.github.com>
2026-02-01 14:11:36 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,118 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+// --- firstPromise ---
+const promise1 = new Promise((resolve, reject) => {
+  let settled = false;
+
+  const clickHandler = (e) => {
+    if (settled) {
+      return;
+    }
+
+    if (e.button === 0) {
+      settled = true;
+      clearTimeout(timeoutId);
+      body.removeEventListener('click', clickHandler);
+      resolve('First promise was resolved on a left click in the document');
+    }
+  };
+
+  body.addEventListener('click', clickHandler);
+
+  const timeoutId = setTimeout(() => {
+    if (settled) {
+      return;
+    }
+    settled = true;
+    body.removeEventListener('click', clickHandler);
+    reject(new Error('First promise was rejected in 3 seconds if not clicked'));
+  }, 3000);
+});
+
+promise1
+  .then((msg) => showNotification(msg, 'success'))
+  .catch((err) => showNotification(err.message, 'error'));
+
+// --- secondPromise ---
+const promise2 = new Promise((resolve, reject) => {
+  let settled = false;
+
+  const handler = (e) => {
+    if (settled) {
+      return;
+    }
+
+    if (e.button === 0 || e.button === 2) {
+      settled = true;
+      clearTimeout(timeoutId);
+      body.removeEventListener('mousedown', handler);
+      resolve('Second promise was resolved');
+    }
+  };
+
+  body.addEventListener('mousedown', handler);
+
+  const timeoutId = setTimeout(() => {
+    if (settled) {
+      return;
+    }
+    settled = true;
+    body.removeEventListener('mousedown', handler);
+
+    reject(
+      new Error('Second promise was not resolved because no click happened'),
+    );
+  }, 3000);
+});
+
+promise2
+  .then((msg) => showNotification(msg, 'success'))
+  .catch((err) => showNotification(err.message, 'error'));
+
+// --- thirdPromise ---
+const promise3 = new Promise((resolve, reject) => {
+  const settled = false;
+  let left = false;
+  let right = false;
+
+  const handler = (e) => {
+    if (settled) {
+      return;
+    }
+
+    if (e.button === 0) {
+      left = true;
+    }
+
+    if (e.button === 2) {
+      right = true;
+    }
+
+    if (left && right) {
+      body.removeEventListener('mousedown', handler);
+
+      resolve(
+        // eslint-disable-next-line max-len
+        'Third promise was resolved only after both left and right clicks happened',
+      );
+    }
+  };
+
+  body.addEventListener('mousedown', handler);
+});
+
+promise3
+  .then((msg) => showNotification(msg, 'success'))
+  .catch((err) => showNotification(err.message, 'error'));
+
+// --- helper function ---
+function showNotification(text, type) {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.classList.add(type);
+  div.textContent = text;
+  body.appendChild(div);
+}
